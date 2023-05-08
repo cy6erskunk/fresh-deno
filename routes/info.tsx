43b57c4bfd78db6a2990bb85e9f5f818b4672b17
Igor Shevchenko <cyberskunk@gmail.com>
2023-05-08 21:43:54 +0300

    Try out a handler

--- routes/info.tsx
+++ routes/info.tsx
@@ -1,3 +1,15 @@
+// routes/info.tsx
+
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+    async GET(req, ctx) {
+        const resp = await ctx.render();
+        resp.headers.set("X-Fresh-Header", `so fresh (from ${req.referrer})!`);
+        return resp;
+    }  
+} 
+
 export default function() {
     return (
         <main class="max-w-screen-md mx-auto">
