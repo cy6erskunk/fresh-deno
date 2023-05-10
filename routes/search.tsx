7bc9bab42576a9619491f3a8c7b88dc2bcb32ac2
Igor Shevchenko <cyberskunk@gmail.com>
2023-05-10 12:17:50 +0300

    Add trivial form example

---
+++ routes/search.tsx
@@ -0,0 +1,26 @@
+// routes/search.ctx
+import { Handlers, PageProps } from "https://deno.land/x/fresh@1.1.5/server.ts";
+
+interface Data {
+    query: string
+}
+
+export const hander: Handlers<Data> = {
+    GET(req, ctx) {
+        const url = new URL(req.url);
+        const query = url.searchParams.get('q') || '';
+        return ctx.render({query});
+    }
+}
+
+export default function Search({data = {query: ''}}: PageProps<Data> ) {
+    const { query } = data;
+    return (
+        <main class="p-4 mx-auto max-w-screen-md">
+            <form method="GET">
+                <input name="q" type="text" value={query}/>
+                <input type="submit"/>
+            </form>
+        </main>
+    )
+}
