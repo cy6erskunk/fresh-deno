c4a29a3e2bed4afd31e415c3227055f8e48d13d2
Igor Shevchenko <cyberskunk@gmail.com>
2023-05-10 20:15:25 +0300

    Fix search page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,20 +4,22 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/info.tsx";
-import * as $4 from "./routes/search.tsx";
+import * as $1 from "./routes/_500.tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/info.tsx";
+import * as $5 from "./routes/search.tsx";
 import * as $$0 from "./islands/Countdown.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/info.tsx": $3,
-    "./routes/search.tsx": $4,
+    "./routes/_500.tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/info.tsx": $4,
+    "./routes/search.tsx": $5,
   },
   islands: {
     "./islands/Countdown.tsx": $$0,
